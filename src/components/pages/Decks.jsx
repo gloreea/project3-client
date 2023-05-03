1a20b29ee4c1e7e68d89505235d1ef5eb421fd04
EGORDYU <egordyuzhev@gmail.com>
2023-05-03 15:25:33 -0600

    pinged backend

--- src/components/pages/Decks.jsx
+++ src/components/pages/Decks.jsx
@@ -6,6 +6,34 @@ import axios from 'axios';
 
 export default function Decks(){
     const [deck, setDeck] = useState({})
+
+    const {id} = useParams()
+    const navigate = useNavigate()
+
+
+    
+
+    useEffect(() => {
+        const token = localStorage.getItem('jwt');
+        console.log(token)
+
+        if (!token) {
+            navigate('/login'); // Redirect user to login page if no token found
+          }
+
+
+        axios.get(`${process.env.REACT_APP_SERVER_URL}/api-v1/decks/1`, {
+            headers: {
+                'Authorization': token
+            }
+        })
+        .then(response => {
+            console.log(response)
+            setDeck(response.data.result)
+        })
+        .catch(console.warn)
+    },[])
+
     return(
         <>
         This is the page to show the user their collection of Decks
