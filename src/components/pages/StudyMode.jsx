dfc69bf4316af2ee4535353008d73fc7f5ac0470
gloreea <aranda_gloree408@icloud.com>
2023-05-04 15:04:20 -0700

    study mode page being made to display flash cards

--- src/components/pages/StudyMode.jsx
+++ src/components/pages/StudyMode.jsx
@@ -7,6 +7,7 @@ import axios from 'axios';
 export default function StudyMode(){
   const [cards, setCards] = useState([]);
   const {id} = useParams()
+  const [currentCardIndex, setCurrentCardIndex]=useState(0)
   const navigate = useNavigate();
 
   const fetchCards = async () => {
@@ -35,24 +36,42 @@ export default function StudyMode(){
 
   
 
-  const toggleBack = (event) => {
-    const backElement = event.currentTarget.querySelector(".flashcard-container-back");
-    backElement.classList.toggle("flashcard-container-show-back-back");
-  };
+//   const toggleBack = (event) => {
+//     const backElement = event.currentTarget.querySelector(".flashcard-container-back");
+//     backElement.classList.toggle("flashcard-container-show-back-back");
+//   };
   
-  const flashCard = cards.map(card => (
-    <div className="flashcard-container" key={card._id} onClick={toggleBack}>
-      <p className="flashcard-container-p flashcard-container-front">Front: {card.front}</p>
-      <p className="flashcard-container-back ">Back: {card.back}</p>
-    </div>
-  ));
-  
-  
-  
+//   const flashCard = cards.map(card => (
+//     <div className="flashcard-container" key={card._id} onClick={toggleBack}>
+//       <p className="flashcard-container-p flashcard-container-front">Front: {card.front}</p>
+//       <p className="flashcard-container-back ">Back: {card.back}</p>
+//     </div>
+//   ));
+  const handleNextCard = () => {
+    setCurrentCardIndex((prevIndex) => prevIndex + 1);
+  }
+  const currentCard = cards[currentCardIndex]
+  if (currentCardIndex >= cards.length) {
+    return (
+        <div>
+            <h2>No more flashcards</h2>
+        </div>
+    )
+  }
 
   return (
-    <>
-      {flashCard}
-    </>
+    <div>
+      <div>
+        <h2>Study Mode</h2>
+        <div>
+          <h2>Current Card {currentCardIndex + 1}</h2>
+          <p>Front: {currentCard.front}</p>
+          <p>Back: {currentCard.back}</p>
+        </div>
+        <div>
+          <button onClick={handleNextCard}>Next Card</button>
+        </div>
+      </div>
+    </div>
   );
 }
